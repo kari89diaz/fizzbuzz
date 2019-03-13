896e6ac1a1a1baec4f678ef878d472c89a1afd95
kari89diaz <kari89diaz@gmail.com>
2019-03-13 15:09:39 -0300

    FizzBuzz Practice

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 0; num < 100; num++)
+            {
+                if ((num % 3 == 0) && (num % 5 == 0))
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
